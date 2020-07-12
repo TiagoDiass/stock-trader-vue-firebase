6eeb4eb8f9c2f09cb8aa544e0f201d3a21b1f0e4
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-11 21:10:47 -0300

    Incrementando projeto, componente stock feito

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import router from './router';
+import './plugins/vuetify';
+import './plugins/sweetAlert';
+
+Vue.config.productionTip = false;
 
 new Vue({
-	render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App),
+}).$mount('#app');

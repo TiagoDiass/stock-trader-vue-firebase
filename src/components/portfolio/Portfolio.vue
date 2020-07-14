bf40d4dc2a2bff286ae81005510a53f93c8b0476
TiagoDiass <tiago.costadiasss@gmail.com>
2020-07-14 17:00:11 -0300

    Mudando alerta para tipo info

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -14,7 +14,7 @@ export default {
   mounted() {
     if (!this.stocks.length) {
       this.$swal({
-        icon: 'error',
+        icon: 'info',
         title: 'Ops...',
         text:
           'Parece que você não tem nenhuma ação, compre algumas e depois volte aqui para vê-lás',
